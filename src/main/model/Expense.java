7112096aca77dda8a9708e93710f24e437c604c5
Olivia Shiqi Mai <omai01@students.cs.ubc.ca>
2026-02-12 21:00:00 -0800

    Added implementation of Expense class

--- src/main/model/Expense.java
+++ src/main/model/Expense.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 /**
  * Represents a single expense made during a trip.
@@ -9,56 +8,70 @@ import java.time.LocalTime;
  * description.
  */
 public class Expense {
+    private LocalDate date;
+    private double amount;
+    private ExpenseCategory category;
+    private String description;
+
     // EFFECTS: constructs an expense with the given date, amount, category, and
     // description.
     // throws IllegalArgumentException if amount <= 0.
     public Expense(LocalDate date, double amount,
             ExpenseCategory category, String description) {
-        // stub
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive.");
+        }
+        this.date = date;
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
     }
 
     // EFFECTS: returns the date of this expense.
     public LocalDate getDate() {
-        return null; // stub
+        return date;
     }
 
     // EFFECTS: returns the amount of this expense.
     public double getAmount() {
-        return 0.0;
+        return amount;
     }
 
     // EFFECTS: returns the category of this expense.
     public ExpenseCategory getCategory() {
-        return null;
+        return category;
     }
 
     // EFFECTS: returns the description of this expense; may be null.
     public String getDescription() {
-        return null;
+        return description;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the amount of this expense to amount.
     // throws IllegalArgumentException if amount <= 0.
     public void setAmount(double amount) {
-        // stub
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive.");
+        }
+        this.amount = amount;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the date of this expense to date.
     public void setDate(LocalDate date) {
-        // stub
+        this.date = date;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the category of this expense to category.
     public void setCategory(ExpenseCategory category) {
-        // stub
+        this.category = category;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the description of this expense to description (may be null).
     public void setDescription(String description) {
-        // stub
+        this.description = description;
     }
 }
