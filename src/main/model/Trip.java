15bef0dae7d966fff6704f2a1fcbeaf82858ac7a
Olivia Shiqi Mai <omai01@students.cs.ubc.ca>
2026-02-13 12:30:57 -0800

    Added implementation of Trip class

--- src/main/model/Trip.java
+++ src/main/model/Trip.java
@@ -5,37 +5,44 @@ package model;
  * A trip contains an itinerary, an expense log, and a checklist.
  */
 public class Trip {
+    private String name;
+    private Itinerary itinerary;
+    private ExpenseLog expenseLog;
+    private Checklist checklist;
 
     // EFFECTS: constructs a trip with the given name.
     // Initializes an empty itinerary, expense log,
     // and checklist for this trip.
     public Trip(String name) {
-        // stub
+        this.name = name;
+        this.itinerary = new Itinerary();
+        this.expenseLog = new ExpenseLog();
+        this.checklist = new Checklist();
     }
 
     // EFFECTS: returns the name of this trip.
     public String getName() {
-        return null;
+        return name;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the name of this trip to the given name.
     public void setName(String name) {
-        // stub
+        this.name = name;
     }
 
     // EFFECTS: returns the itinerary associated with this trip.
     public Itinerary getItinerary() {
-        return null;
+        return itinerary;
     }
 
     // EFFECTS: returns the expense log associated with this trip.
     public ExpenseLog getExpenseLog() {
-        return null;
+        return expenseLog;
     }
 
     // EFFECTS: returns the checklist associated with this trip.
     public Checklist getChecklist() {
-        return null;
+        return checklist;
     }
 }
