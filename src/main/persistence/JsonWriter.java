261ef5d1cc25e47861d0a2a04e445c6ab19e2bc9
Olivia Shiqi Mai <omai01@students.cs.ubc.ca>
2026-03-01 08:12:33 -0800

    Added specification of JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,44 @@
 package persistence;
 
-public class JsonWriter {
+import model.Trip;
+import org.json.JSONObject;
 
+import java.io.*;
+
+// Represents a writer that writes JSON representation of trip to file
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file
+    // cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of workroom to file
+    public void write(Trip trip) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        // stub
+    }
 }
